cbc82dd9c29f9f36098b826e477893d6bad93643
haneunchae <moomoo08@kakao.com>
2025-04-20 01:28:21 +0900

    🧠 index.js 구조 수정 및 초기 렌더링 확인

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
-// 여기에 우리가 만든 전체 App 코드가 들어갈 예정입니다
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>정신병동 수능 플랜에 오신 걸 환영합니다 🧠</h1>
+    </div>
+  );
+}
+
+export default App;
